189fcaa4765bf110893447211d00b87b9856b452
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-08-20 17:21:14 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProjetoLetsCode2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Menu menu = new Menu(); //Instaciado o Menu pra que o mesmo funcione pois nao foi declarado
+        int opcao = menu.menu(); // como static.
+
+       Console.WriteLine($"A opção do menu é : {opcao}");
+
+      
+       
+        }
+    }
+}
